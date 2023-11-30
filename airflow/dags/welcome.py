dc60c34f5a7fa43daa8707631323dfc024ed3a57
D. Purge <david.purge@gmail.com>
2023-11-30 12:22:39 +0100

    Play with welcome dag

--- airflow/dags/welcome.py
+++ airflow/dags/welcome.py
@@ -2,10 +2,19 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
-from datetime import datetime
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=15),
+}
 
 with DAG(
-    "welcome",
+    'welcome',
+    default_args = default_args,
+    description = 'Exaple pipeline that prints out "Hello, World!"',
     start_date = datetime(2023, 11, 30),
     schedule = '@daily',
     catchup = False):
