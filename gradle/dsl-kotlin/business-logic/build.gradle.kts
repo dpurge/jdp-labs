65784edc9c2e61a97d29e4934101113ba98f6e60
D. Purge <david.purge@gmail.com>
2024-01-09 15:34:55 +0100

    Version management

--- gradle/dsl-kotlin/business-logic/build.gradle.kts
+++ gradle/dsl-kotlin/business-logic/build.gradle.kts
@@ -7,7 +7,9 @@ dependencies {
 
     implementation(project(":data-model"))
 
-    implementation("org.apache.commons:commons-lang3")
-    implementation("org.slf4j:slf4j-api")
-    implementation("org.apache.commons:commons-lang3")
+    // implementation("org.apache.commons:commons-lang3")
+    // implementation("org.slf4j:slf4j-api")
+    
+    implementation(libs.commons.lang)
+    implementation(libs.slf4j.api)
 }
